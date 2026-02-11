7a24f5521ded37e74dfec91b278d7dd39f460a9a
dpadhy1 <debeshpadhy99@gmail.com>
2026-02-11 13:15:10 +0530

    Added different services

---
+++ application-ms/src/test/java/com/debesh/snaphire/application_ms/ApplicationMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debesh.snaphire.application_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ job-ms/src/test/java/com/debesh/snaphire/job_ms/JobMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debesh.snaphire.job_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
